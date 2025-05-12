5297e26e79f0fdaaa9c2e2ecc1a5d9f44f5a899c
Jawwad <xgf.wanse@yahoo.com>
2025-05-12 15:02:19 +0300

    lil bro

---
+++ assets/data/masterItems.js
@@ -0,0 +1,52 @@
+const masterItems = {
+	costumes: [{
+		title: "Costume Preview",
+		img: "assets/img/jawwad_mini.jpg",
+		full: "assets/img/jawwad.jpg"
+	}],
+	hairstyles: [{
+		title: "Hairstyle Preview",
+		img: "assets/img/jawwad_mini.jpg",
+		full: "assets/img/jawwad.jpg"
+	}],
+	weapons: [{
+		title: "Weapon Preview",
+		img: "assets/img/jawwad_mini.jpg",
+		full: "assets/img/jawwad.jpg"
+	}],
+	others: [{
+		title: "Other Asset Preview",
+		img: "assets/img/jawwad_mini.jpg",
+		full: "assets/img/jawwad.jpg"
+	}],
+	free: [{
+		title: "Antaras Weapons",
+		img: "assets/img/antaras_weapons.jpg",
+		full: "assets/img/antaras_weapons.jpg"
+	}, {
+		title: "Dynasty Weapons",
+		img: "assets/img/dynasty_weapons.jpg",
+		full: "assets/img/dynasty_weapons.jpg"
+	}, {
+		title: "God Awakening Weapons",
+		img: "assets/img/god_awakening_weapons.jpg",
+		full: "assets/img/god_awakening_weapons.jpg"
+	}, {
+		title: "Silver Hunter Weapons",
+		img: "assets/img/silver_hunter_weapons.jpg",
+		full: "assets/img/silver_hunter_weapons.jpg"
+	}, {
+		title: "Dark Crystal Weapons",
+		img: "assets/img/dark_crystal_weapons.jpg",
+		full: "assets/img/dark_crystal_weapons.jpg"
+	}],
+	tutorials: [{
+		title: "Installing Assets",
+		youtube: "dQw4w9WgXcQ"
+	}, {
+		title: "Editing Textures",
+		youtube: "abcdEFGH123"
+	}]
+};
+
+export default masterItems;
